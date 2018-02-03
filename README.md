7b31917904fa95ab18dcc111e636b12d3a542f86
Ben Brooks <shards@gmail.com>
2018-02-03 17:36:57 +1300

    Added in production boost calculations

---
+++ README.md
@@ -0,0 +1,5 @@
+﻿# RvtCalc
+Rocket Valley Tycoon Calculator
+
+A simple calculator to work out resources required for the mobile game Rocket Valley Tycoon
+https://play.google.com/store/apps/details?id=com.rocketcolastudio.rvtih&hl=en
--- RocketValleyCalc.js
+++ RocketValleyCalc.js
@@ -1,6 +1,10 @@
 ﻿$(function () {
     ListResources();
     SetParamsFromUrl();
+
+    $(window).on('popstate', function (event) {
+        SetParamsFromUrl();
+    });
 });
 
 function ListResources() {
@@ -10,33 +14,40 @@ function ListResources() {
     }
     keys.sort();
 
+    $('#Resource').empty();
+    $('#Resource').append('<option value="">Select resource...</option>');
     for (var i in keys) {
         var resourceKey = keys[i];
         var resource = Resources[resourceKey];
         var mined = IsMined(resourceKey);
         if (!mined) {
-            var resListDiv = '<div class="Resource"><input type="hidden" class="ResourceKey" value="' + resourceKey + '" />' + resource.Name + '</div>';
-            $("#ResourcesList").append(resListDiv);
+            var resOption = '<option value="' + resourceKey + '">' + resource.Name + '</option>';
+            $("#Resource").append(resOption);
         }
     }
 
-    $(document).on('click', '.Resource', function (resource) {
-        var resourceKey = $(resource.target).find('.ResourceKey').val();
-        Calculate(resourceKey);
+    $('.Options').change(function () {
+        SetWindowUrl();
+        Calculate();
     });
 
 }
 
-function Calculate(resourceKey) {
+function Calculate() {
+    var resourceKey = $('#Resource').val();
     var amount = parseFloat($("#AmountWanted").val());
     var period = parseFloat($('#Period').val());
     var periodType = $('#PeriodType').val();
+    var colaBoost = $('#ColaBoost').val();
+    var techBoost = $('#TechBoost').val();
     var totals = {};
-
     var amountAdjusted = AmountToAdjusted(amount, period, periodType);
 
-    SetWindowUrl(resourceKey, amount, period, periodType);
-    CalculateRequired(resourceKey, amountAdjusted, totals);
+    if (resourceKey === null || resourceKey.length < 1) {
+        return;
+    }
+
+    CalculateRequired(resourceKey, amountAdjusted, totals, colaBoost, techBoost);
     PrintRequired(resourceKey, amount, period, periodType, totals);
 }
 
@@ -74,6 +85,8 @@ function SetParamsFromUrl() {
     var amount = params.get('amount');
     var period = params.get('period');
     var periodType = params.get('periodType');
+    var colaBoost = params.get('colaBoost');
+    var techBoost = params.get('techBoost');
 
     if (amount !== null) {
         $("#AmountWanted").val(amount);
@@ -87,20 +100,35 @@ function SetParamsFromUrl() {
         $("#PeriodType").val(periodType);
     }
 
+    if (colaBoost !== null) {
+        $("#ColaBoost").val(colaBoost);
+    }
+
+    if (techBoost !== null) {
+        $("#TechBoost").val(techBoost);
+    }
+
     if (resourceKey !== null) {
-        Calculate(resourceKey);
+        $("#Resource").val(resourceKey);
+        Calculate();
     }
 }
 
-function GetUrl(resourceKey, amount, period, periodType) {
+function GetUrl(resourceKey, amount, period, periodType, colaBoost, techBoost) {
     var baseUrl = location.protocol + '//' + location.host + location.pathname;
-    var newUrl = baseUrl + '?resource=' + resourceKey + '&amount=' + amount + '&period=' + period + '&periodType=' + periodType;
+    var newUrl = baseUrl + '?resource=' + resourceKey + '&amount=' + amount + '&period=' + period + '&periodType=' + periodType + '&colaBoost=' + colaBoost + '&techBoost=' + techBoost;
     return newUrl;
 }
 
-function SetWindowUrl(resourceKey, amount, period, periodType) {
-    var newUrl = GetUrl(resourceKey, amount, period, periodType);
-    window.history.replaceState({}, "", newUrl);
+function SetWindowUrl() {
+    var resourceKey = $('#Resource').val();
+    var amount = parseFloat($("#AmountWanted").val());
+    var period = parseFloat($('#Period').val());
+    var periodType = $('#PeriodType').val();
+    var colaBoost = $('#ColaBoost').val();
+    var techBoost = $('#TechBoost').val();
+    var newUrl = GetUrl(resourceKey, amount, period, periodType, colaBoost, techBoost);
+    window.history.pushState({}, "", newUrl);
 }
 
 function IsMined(resourceKey) {
@@ -114,10 +142,8 @@ function PrintRequired(resourceKey, amount, period, periodType, totals) {
     }
     keys.sort();
 
-    var requiredMinedList = $("#RequiredMinedList");
-    var requiredBuiltList = $("#RequiredBuiltList");
-    requiredMinedList.empty();
-    requiredBuiltList.empty();
+    $("#RequiredMinedList").empty();
+    $("#RequiredBuiltList").empty();
 
     $("#Required").css('display', 'table-cell');
     var periodPlural = '';
@@ -125,57 +151,89 @@ function PrintRequired(resourceKey, amount, period, periodType, totals) {
         periodPlural = 's';
     }
     $("#Required").find("h4").text("Creating " + amount + " " + Resources[resourceKey].Name + " per " + period + " " + periodType + periodPlural + " requires (per second):");
-    
+
+    document.title = Resources[resourceKey].Name + ' - RVT Calc';
+
     for (var i in keys) {
         var key = keys[i];
-        var name = Resources[key].Name;
-        var requiredAmount = totals[key].Amount;
-        var requiredFrom = "";
-        for (var fromKey in totals[key].From) {
-            requiredFrom += Resources[fromKey].Name + ': ' + totals[key].From[fromKey].Amount.toLocaleString() + '\n';
-        }
-
-        var mined = IsMined(key);
-        var requiredList = null;
-        if (mined) {
-            requiredList = requiredMinedList;
-        }
-        else {
-            requiredList = requiredBuiltList;
-        }
-
-        var approxMaxLevel = 1000;
-        if (Resources[key].ApproxMaxLevel !== undefined) {
-            approxMaxLevel = Resources[key].ApproxMaxLevel;
-        }
-
-        var displayAmount = requiredAmount;
-        if (displayAmount < 0.001) {
-            displayAmount = '<0.001';
-        }
-
-        var unadjustedAmount = AmountFromAdjusted(requiredAmount, $("#Period").val(), $("#PeriodType").val());
-
-        var url = GetUrl(key, unadjustedAmount, $("#Period").val(), $("#PeriodType").val());
-        var approxMines = '';
-        if (mined) {
-            approxMines = requiredAmount / approxMaxLevel / Resources[key].Produced;
-            if (approxMines < 0.001) {
-                approxMines = '<0.001';
-            }
-        }
-        var localeOptions = { minimumFractionDigits: 3 };
-        var reqTr = '<tr title="' + requiredFrom + '">';
-        reqTr += '<td><a href="' + url + '">' + name + '</a></td>';
-        reqTr += '<td style="text-align: right;">' + displayAmount.toLocaleString('en-GB', localeOptions) + '</td>';
-        reqTr += '<td style="text-align: right;">' + approxMines.toLocaleString('en-GB', localeOptions) + '</td>';
-        reqTr += '</tr>';
-        requiredList.append(reqTr);
-        
+        PrintRequiredResource(key, totals);
     }
+
+    if ($('#RequiredBuiltList').children().length === 0) {
+        $('#ReqiredFactories').hide();
+    }
+    else {
+        $('#ReqiredFactories').show();
+    }
+
+    $('.Factory').on('click', function (event) {
+        if (event.ctrlKey) {
+            return;
+        }
+        event.preventDefault();
+        var key = $(event.target).find('.FactoryKey').val();
+        var amount = $(event.target).find('.FactoryAmount').val();
+        $("#AmountWanted").val(amount);
+        $("#Resource").val(key);
+        SetWindowUrl();
+        Calculate();
+    });
 }
 
-function CalculateRequired(resourceKey, amount, totals) {
+function PrintRequiredResource(key, totals) {
+    var name = Resources[key].Name;
+    var requiredAmount = totals[key].Amount;
+    var requiredFrom = "Of " + requiredAmount.toLocaleString() + " " + name + ";\n";
+    for (var fromKey in totals[key].From) {
+        requiredFrom += ' &bull; ' + totals[key].From[fromKey].Amount.toLocaleString() + ' is required to make ' + Resources[fromKey].Name + '\n';
+    }
+
+    var displayAmount = requiredAmount;
+    if (displayAmount < 0.001) {
+        displayAmount = '<0.001';
+    }
+
+    var approxMaxLevel = 1000;
+    if (Resources[key].ApproxMaxLevel !== undefined) {
+        approxMaxLevel = Resources[key].ApproxMaxLevel;
+    }
+
+    var unadjustedAmount = AmountFromAdjusted(requiredAmount, $("#Period").val(), $("#PeriodType").val());
+
+    var url = GetUrl(key, unadjustedAmount, $("#Period").val(), $("#PeriodType").val(), $("#ColaBoost").val(), $("#TechBoost").val());
+    var approxMines = '';
+    var mined = IsMined(key);
+    if (mined) {
+        approxMines = requiredAmount / approxMaxLevel / Resources[key].Produced;
+        if (approxMines < 0.001) {
+            approxMines = '<0.001';
+        }
+    }
+
+    var localeOptions = { minimumFractionDigits: 3 };
+    var reqTr = '<tr title="' + requiredFrom + '">';
+    if (mined) {
+        reqTr += '<td>' + name + '</td>';
+    }
+    else {
+        reqTr += '<td><a class="Factory" href="' + url + '">';
+        reqTr += '<input type="hidden" class="FactoryKey" value="' + key + '" />';
+        reqTr += '<input type="hidden" class="FactoryAmount" value="' + unadjustedAmount + '" />';
+        reqTr += name + '</a></td>';
+    }
+    reqTr += '<td class="NumberCell">' + displayAmount.toLocaleString('en-GB', localeOptions) + '</td>';
+    reqTr += '<td class="NumberCell">' + approxMines.toLocaleString('en-GB', localeOptions) + '</td>';
+    reqTr += '</tr>';
+    if (mined) {
+        $("#RequiredMinedList").append(reqTr);
+    }
+    else {
+        $("#RequiredBuiltList").append(reqTr);
+    }
+
+}
+
+function CalculateRequired(resourceKey, amount, totals, colaBoost, techBoost) {
     var resource = Resources[resourceKey];
 
     if (resource === undefined) {
@@ -186,7 +244,7 @@ function CalculateRequired(resourceKey, amount, totals) {
     var producedPerLevel = Resources[resourceKey].Produced;
 
     for (var requiredKey in resource.Required) {
-        var requiredAmount = resource.Required[requiredKey] * amount / producedPerLevel;
+        var requiredAmount = resource.Required[requiredKey] * amount / producedPerLevel / colaBoost / (techBoost / 100);
 
         if (!(requiredKey in totals)) {
             totals[requiredKey] = {};
@@ -206,7 +264,7 @@ function CalculateRequired(resourceKey, amount, totals) {
             return;
         }
 
-        CalculateRequired(requiredKey, requiredAmount, totals);
+        CalculateRequired(requiredKey, requiredAmount, totals, colaBoost, techBoost);
     }
 
 }
